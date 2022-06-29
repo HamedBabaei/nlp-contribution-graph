cd8dc74e6147a885e3496be0d77c8f210e11b697
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2022-06-29 19:08:54 +0430

    [update] update model configs with general params

--- configuration/model_config.py
+++ configuration/model_config.py
@@ -17,16 +17,19 @@ class ModelConfig:
                                 default="assets/tfidf_ml_clf.sav", 
                                 help='Path to trained tfidf+ml classifier for sentence classifiation')
         self.parser.add_argument("--summarizer_model_path", type=str, 
-                                default="assets/t5-small/", 
+                                default="assets/sum-t5-small/", 
                                 help='Path to fine-tuned T5 summarizer')
         self.parser.add_argument("--output_dir", type=str, 
-                                default="dataset/outputs/summarizer (t5-small) - classifier (tfidf+xgboost)", 
+                                default="outputs", 
                                 help='Path to output directory to save outputs')
         self.parser.add_argument("--text_index_th", type=int, default=50, 
                                 help='index threshold')
         self.parser.add_argument("--text_lenght_th", type=int, default=3, 
                                 help='text lenght threshold')
-                                
+        self.parser.add_argument("--modelname", type=str, default="clf:xgboost-ie:t5small", 
+                                help='name of classifier for sentence classifications and output dir'\
+                                    '[clf:xgboost-ie:t5small, clf:t5small-ie:t5small, clf:t5large-ie:t5large]')
+        
         self.parser.add_argument("--seed", type=int, default=422, help='random seeds')
         self.parser.add_argument("-f")
 
