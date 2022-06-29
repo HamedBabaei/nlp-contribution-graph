80a4a349f3c87f9baf99ee9cd2bce1dbef309f71
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2022-06-29 19:09:20 +0430

    [update] src __init__ with new classes

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,6 @@
     Make the importing much shorter
 """
 from .classifier import RPSentenceDetector
+from .classifier import TFIDFXGBoost
 from .summaizer import SummarizerInference
+from .evaluation import NLPContributionGraphEval
